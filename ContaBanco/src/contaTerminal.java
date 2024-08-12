d7ffc935ce077a7de4911c8d844d5f4a0e196922
jeff17 <mkjeff17@gmail.com>
2024-08-12 12:05:37 -0300

    projeto dio banco java

--- ContaBanco/src/contaTerminal.java
+++ ContaBanco/src/contaTerminal.java
@@ -1,5 +1,24 @@
-public class contaTerminal {
+import java.util.Scanner;
+
+public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+ 
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Bem Vindo!, por favor digite o número da agencia: ");
+        String agencia = sc.nextLine();
+
+        System.out.println("Digite o seu nome: ");
+        String nome = sc.nextLine();
+
+        System.out.println("Digite o número da conta: ");
+        int numero = sc.nextInt();
+
+        System.out.println("Digite o saldo para depósito: ");
+        double saldo = sc.nextDouble();
+        
+        System.out.println("Olá "+nome+", obrigado por criar sua conta, sua agência é "+agencia+", conta "+numero+", seu saldo "+saldo+", já disponível para saque.");
+        
+        sc.close();
     }
 }
